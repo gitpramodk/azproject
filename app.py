57e04953570acad9d95e75eaaba1cadbb3f873a7
gitpramodk <58698650+gitpramodk@users.noreply.github.com>
2022-03-13 15:19:22 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,53 +1,50 @@
-#!/usr/bin/env python
-# coding: utf-8
+from flask import Flask, request, redirect, render_template 
 
-# In[1]:
-import os
-from flask import Flask, request, redirect, url_for, render_template
-from werkzeug.utils import secure_filename
-from azure.storage.blob import *
-import string
-import random
-import requests
+import os   
+from azure.storage.blob import BlobServiceClient
 import configparser
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__) 
 
-Config = configparser.ConfigParser()
-Config.read("config.py")
+config = configparser.ConfigParser()
+config.read("config.py")
 
-# Account name
-account = Config.get('DEFAULT', 'account')
-# Azure Storage account access key
-key = Config.get('DEFAULT', 'key')
+# Account Connection String
+connect_str = config['DEFAULT']['connect_str']
 # Container name
-container = Config.get('DEFAULT', 'container')
+container_name = config['DEFAULT'] ['container_name']
+
+blob_service_client = BlobServiceClient.from_connection_string(conn_str=connect_str) # create a blob service client to interact with the storage account
+
+try:
+    container_client = blob_service_client.get_container_client(container=container_name) # get container client to interact with the container in which images will be stored
+    container_client.get_container_properties() # get properties of the container to force exception to be thrown if container does not exist
+except Exception as e:
+    container_client = blob_service_client.create_container(container_name) # create a container in the storage account if it does not exist
 
 
-blob_service = BlockBlobService(account_name=account, account_key=key)
-
-@app.route("/")
-def main():
+#flask endpoint for Home page of upload
+@app.route("/") 
+def main():  
     return render_template('index.html')
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        fileextension = filename.rsplit('.', 1)[1]
+
+#flask endpoint to upload a photo  
+@app.route("/upload", methods=["POST"])  
+def process_upload():  
+    filenames = ""  
+    for file in request.files.getlist("photos"):
         try:
-            blob_service.create_blob_from_stream(container, filename, file)
+            container_client.upload_blob(file.filename, file) # upload the file to the container using the filename as the blob name
+            filenames += file.filename + "<br /> "
+        except Exception as e:
+            print(e)
+            print("Ignoring duplicate filenames") # ignore duplicate filenames
 
-        except Exception:
-            print('Exception=' + Exception)
-            pass
-        ref = 'http://' + account + '.blob.core.windows.net/' + container + '/' + filename
 
-    return render_template('uploadfile.html')
+    return render_template('upload.html') 
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
 
 
-# In[ ]:
